01608c212caa0c2f15c9ebd88c445c5e9e33c2b5
Michael Rodgers <38140530+michaelrodgers@users.noreply.github.com>
2018-11-25 21:30:42 -0800

    Initial Commit of .java files

---
+++ SnapShopConfiguration.java
@@ -0,0 +1,23 @@
+// Write your short report here (-2 if there is no report)
+
+/**
+ * A class to configure the SnapShop application
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SnapShopConfiguration
+{
+  /**
+   * Method to configure the SnapShop.  Call methods like addFilter
+   * and setDefaultFilename here.
+   * @param theShop A pointer to the application
+   */
+  public static void configure(SnapShop theShop)
+  {
+
+    theShop.setDefaultFilename("c:/hw5/billg.jpg");
+    theShop.addFilter(new FlipHorizontalFilter(), "Flip Horizontal");
+    // add your other filters below
+  }
+}
